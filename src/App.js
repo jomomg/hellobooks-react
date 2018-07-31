f67e91d8f48760d0bc35251b3a2ff4ae1750a477
Jomo <jomomg@gmail.com>
2018-07-31 22:58:52 +0300

    [feature] Add private routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import LandingPage from './components/landing'
 import SignUp from './components/signup'
 import BookPage from './components/books'
+import Auth from './utils/authentication'
 import SignIn from './components/login'
+import UserProfilePage from './components/user_profile'
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={props =>
+            Auth.isAuthenticated ? (
+                <Component {...props} />
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: "/signin",
+                        state: { from: props.location }
+                    }}
+                />
+            )
+        }
+    />
+);
 
 class App extends Component {
   render() {
@@ -14,7 +33,9 @@ class App extends Component {
                  <Route path="/" exact component={LandingPage}/>
                  <Route path="/signup" component={SignUp}/>
                  <Route path="/signin" component={SignIn}/>
-                 <Route path="/books" component={BookPage}/>
+                 <Route path="/books" exact component={BookPage}/>
+                 <PrivateRoute path="/profile" component={UserProfilePage}/>
+                 {/*<Route path="/books/:id" component={SingleBookPage}/>*/}
              </Switch>
          </React.Fragment>
      )

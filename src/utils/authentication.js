37b59e8405b4c493c157e42abb376f2bcb520241
Jomo <jomomg@gmail.com>
2018-08-06 12:14:39 +0300

    [chore] Add function to check token validity

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -1,11 +1,50 @@
+import jwt_decode from 'jwt-decode';
+
+const isTokenValid = ()=> {
+    let isExpired = false;
+    let decoded = jwt_decode(localStorage.getItem('accessToken'));
+    let now = new Date();
+
+    if (decoded.exp < Math.round(now.getTime()/1000)) {
+        isExpired = true
+    }
+    return !isExpired;
+};
 
 const Auth = {
-    isAuthenticated: false,
+    isAuthenticated: isTokenValid(),
+
     authenticate() {
         this.isAuthenticated = true;
     },
-    signout() {
+
+    logout() {
         this.isAuthenticated = false;
+    },
+
+    isAdmin() {
+        try {
+            let accessToken = localStorage.getItem('accessToken');
+            let decoded = jwt_decode(accessToken);
+            console.log(decoded);
+            return decoded.user_claims.is_admin;
+        } catch (e) {
+            console.log(e)
+        }
+    },
+
+    getUserInfo() {
+        try{
+            let accessToken = localStorage.getItem('accessToken');
+            let decoded = jwt_decode(accessToken);
+            return {
+                first_name: decoded.user_claims.first_name,
+                last_name: decoded.user_claims.last_name,
+                email: decoded.identity,
+            }
+        } catch (e) {
+            console.log(e)
+        }
     }
 
 };

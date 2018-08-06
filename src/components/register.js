a1e1a4c82beed9cef017b10f4fc6de70ab90e0a8
Jomo <jomomg@gmail.com>
2018-08-06 12:17:25 +0300

    [chore] Use placeholder prop instead of defaultValue

--- src/components/register.js
+++ src/components/register.js
@@ -1,83 +1,124 @@
 import React, { Component } from 'react'
-import './css/signup.css'
-import {Row, Input, Button} from 'react-materialize'
-import axios from 'axios'
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import {withRouter} from 'react-router-dom';
+import api from '../utils/requests';
 
-const register_url = 'https://hello-kitabu.herokuapp.com/api/v1/auth/register';
+const register_url = 'auth/register';
 
-class SignUp extends Component {
+const inputStyles = {
+    marginLeft: '4%',
+    marginRight: '4%',
+    width: '400px'
+};
+
+class Register extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            first_name: "",
+            last_name: "",
             email: "",
             password: "",
-            "confirm password": "",
+            confirm_password: "",
         }
 
     }
 
-    sendRegisterInfo = async() => {
-        try {
-            const response = await axios.post(register_url, {
-                email: this.state.email,
-                password: this.state.password,
-                "confirm password": this.state["confirm password"]
-            });
-            console.log(response);
-        } catch (e) {
-            console.log(e)
-        }
+    sendRegisterInfo = () => {
+        api.post(register_url, {
+            first_name: this.state.first_name,
+            last_name: this.state.last_name,
+            email: this.state.email,
+            password: this.state.password,
+            confirm_password: this.state.confirm_password
+        }).then(res=>{
+            console.log(res);
+            this.props.history.push('/login')
+        }).catch(err=>console.log(err))
     };
 
     handleClick = () => {
         this.sendRegisterInfo();
     };
 
-    handleChange = (event) => {
-        this.setState({[event.target.name]: event.target.value})
+    handleChange = name => (event) => {
+        this.setState({[name]: event.target.value})
     };
 
     render () {
         return (
-            <div className="signup-form z-depth-3">
-                <div className="form-contents">
-                <Row>
-                    <h5 style={{textAlign: 'center'}}>Sign up for HelloBooks</h5>
-                    <div style={{margin: '10px'}}>
-                        <Input
-                            name="email"
-                            type="email"
-                            label="Email"
-                            s={12}
-                            value={this.state.email}
-                            onChange={this.handleChange}/>
-                        <Input
-                            name="password"
-                            type="password"
-                            label="password"
-                            s={12}
-                            value={this.state.password}
-                            onChange={this.handleChange}/>
-                        <Input
-                            name="confirm password"
-                            type="password"
-                            label="Confirm password"
-                            s={12}
-                            value={this.state["confirm password"]}
-                            onChange={this.handleChange}/>
-                        <div style={{textAlign: 'center'}}>
-                        <button
-                            onClick={this.handleClick}
-                            className="btn waves-effect waves-light"
-                            type="submit"
-                            name="submit">SIGN UP</button>
-                        </div>
-                    </div>
-                </Row>
-                </div>
-            </div>
+            <Paper style={{margin: '5% auto 5% auto', width: 400, height: 480}}>
+                <Typography variant={'headline'} component={'h2'} style={{paddingTop: '3%', textAlign: 'center'}}>
+                    Sign Up for Hello Books
+                </Typography>
+                <form noValidate style={{display: 'flex', flexWrap: 'wrap'}}>
+                    <TextField
+                        style={inputStyles}
+                        required
+                        id="first-name"
+                        label="First Name"
+                        placeholder="First Name"
+                        value={this.state.first_name}
+                        onChange={this.handleChange('first_name')}
+                        margin="normal"
+                    />
+                    <TextField
+                        style={inputStyles}
+                        required
+                        id="last-name"
+                        label="Last Name"
+                        placeholder="Last Name"
+                        value={this.state.last_name}
+                        onChange={this.handleChange('last_name')}
+                        margin="normal"
+                    />
+                    <TextField
+                        style={inputStyles}
+                        required
+                        type={'email'}
+                        id="email"
+                        label="Email"
+                        placeholder="Your email address"
+                        value={this.state.email}
+                        onChange={this.handleChange('email')}
+                        margin="normal"
+                    />
+                    <TextField
+                        style={inputStyles}
+                        type={'password'}
+                        required
+                        id="password"
+                        label="Password"
+                        placeholder="Enter a password"
+                        value={this.state.password}
+                        onChange={this.handleChange('password')}
+                        margin="normal"
+                    />
+                    <TextField
+                        style={inputStyles}
+                        type={'password'}
+                        required
+                        id="confirm-password"
+                        label="Confirm Password"
+                        placeholder="Confirm your password"
+                        value={this.state.confirm_password}
+                        onChange={this.handleChange('confirm_password')}
+                        margin="normal"
+                    />
+                    <Button
+                        onClick={this.handleClick}
+                        variant={'extendedFab'}
+                        color={'primary'}
+                        style={{margin: '4% auto 0% auto', backgroundColor: 'orange', color: 'black'}}
+                    >SIGN UP
+                    </Button>
+                </form>
+            </Paper>
         )
     }
 }
 
-export default SignUp;
+export default withRouter(Register);

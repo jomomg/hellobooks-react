a24386c4265bf5e8780f6a96db9f8647dee70d7f
Jomo <jomomg@gmail.com>
2018-08-08 22:53:40 +0300

    [chore] Store authentication status in local storage

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -1,49 +1,34 @@
 import jwt_decode from 'jwt-decode';
 
-const isTokenValid = ()=> {
-    let isExpired = false;
+const isTokenExpired = ()=> {
     let decoded = jwt_decode(localStorage.getItem('accessToken'));
     let now = new Date();
-
-    if (decoded.exp < Math.round(now.getTime()/1000)) {
-        isExpired = true
-    }
-    return !isExpired;
+    return decoded.exp < Math.round(now.getTime()) / 1000;
 };
 
 const Auth = {
-    isAuthenticated: isTokenValid(),
-
     authenticate() {
-        this.isAuthenticated = true;
+        localStorage.setItem('isAuthenticated', 'true');
     },
 
     logout() {
-        this.isAuthenticated = false;
+        localStorage.setItem('isAuthenticated', 'false');
     },
+    isAuthenticated: () => JSON.parse(localStorage.getItem('isAuthenticated')) && !isTokenExpired(),
 
     isAdmin() {
-        try {
-            let accessToken = localStorage.getItem('accessToken');
-            let decoded = jwt_decode(accessToken);
-            console.log(decoded);
-            return decoded.user_claims.is_admin;
-        } catch (e) {
-            console.log(e)
-        }
+        let accessToken = localStorage.getItem('accessToken');
+        let decoded = jwt_decode(accessToken);
+        return decoded.user_claims.is_admin;
     },
 
     getUserInfo() {
-        try{
-            let accessToken = localStorage.getItem('accessToken');
-            let decoded = jwt_decode(accessToken);
-            return {
-                first_name: decoded.user_claims.first_name,
-                last_name: decoded.user_claims.last_name,
-                email: decoded.identity,
-            }
-        } catch (e) {
-            console.log(e)
+        let accessToken = localStorage.getItem('accessToken');
+        let decoded = jwt_decode(accessToken);
+        return {
+            first_name: decoded.user_claims.first_name,
+            last_name: decoded.user_claims.last_name,
+            email: decoded.identity,
         }
     }
 

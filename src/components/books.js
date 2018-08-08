e76d49b70a8fbd24bd49e4b739b60ad5e2b6931d
Jomo <jomomg@gmail.com>
2018-08-08 22:55:35 +0300

    [chore] change Link component to Route

--- src/components/books.js
+++ src/components/books.js
@@ -7,7 +7,7 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import {Link} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import api from '../utils/requests';
 import Paper from '@material-ui/core/Paper'
 import TopNav from './navbar'
@@ -24,18 +24,26 @@ const BookCard = (props)=> {
                     image="http://placehold.it/100x200"
                     title="Book Cover"
                 />
-                <CardContent>
-                    <Typography gutterBottom variant="headline" component="h2">
+                <CardContent style={{padding: 8}}>
+                    <div>
+                    <Typography gutterBottom variant="title" component="h4">
                         {props.title}
                     </Typography>
                     <Typography component="p">
                         {props.author}
                     </Typography>
+                    </div>
                 </CardContent>
-                <CardActions>
-                    <Button variant={'outlined'} size="small">
-                        <Link to={`/books/${props.id}`}>More</Link>
+                <CardActions style={{margin: 0}}>
+                    <Route render={({ history }) => (
+                    <Button onClick={()=>history.push(`/books/${props.id}`)}
+                            variant={'outlined'}
+                            size="small"
+                            style={{backgroundColor: 'orange', margin: 0}}
+                    >
+                       More
                     </Button>
+                    )}/>
                 </CardActions>
             </Card>
         </Grid>
@@ -57,17 +65,14 @@ class BookPage extends React.Component {
         this.getBooks();
     }
 
-    getBooks = async ()=> {
-        try {
-            this.setState({loading: true});
-            const response = await api.get(books_url);
-            const books = response.data;
-            this.setState({books, loading: false});
-            console.log(response);
-        } catch (error) {
-            console.error('error: ', error);
-            this.setState({error: `${error}`, loading: false});
-        }
+    getBooks = ()=> {
+        this.setState({loading: true});
+        api.get(books_url)
+            .then(res => this.setState({books: res.data, loading: false}))
+            .catch(err => {
+                console.log('error: ', err);
+                this.setState({error: `${err}`, loading: false});
+            })
     };
 
     render() {
@@ -75,7 +80,7 @@ class BookPage extends React.Component {
         if (loading) {
             return (
                 <div>
-                    <TopNav/>
+                    <TopNav title="All books" {...this.props}/>
                     <LinearProgress variant="query" />
                 </div>
             )
@@ -84,7 +89,7 @@ class BookPage extends React.Component {
         if (error) {
             return (
                 <div>
-                    <TopNav/>
+                    <TopNav title="All books" {...this.props}/>
                     <Paper style={{margin: '10% auto 1% auto', maxWidth: 300, backgroundColor: 'red', textAlign: 'center'}}>
                         <span style={{color: 'white'}}>{error}</span>
                     </Paper>
@@ -94,7 +99,7 @@ class BookPage extends React.Component {
         }
         return (
             <div>
-                <TopNav title={'All Books'}/>
+                <TopNav title="All books" {...this.props}/>
                     <div style={{marginLeft: '10%', marginRight: '5%', marginTop: '1%'}}>
                         <Grid container spacing={24}>
                             {this.state.books.map(book => {
